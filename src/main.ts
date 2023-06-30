448c82b56fc12427e41259a6059e8454e1e221b0
xulihang <xulihanghai@163.com>
2023-06-30 14:09:41 +0800

    implement the barcode reading

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json } from 'express';
+const dbr = require("barcode4nodejs");
+dbr.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==")
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(json({ limit: '50mb'}))
   await app.listen(3000);
 }
+
 bootstrap();
--- src/read-barcodes/read-barcodes.controller.ts
+++ src/read-barcodes/read-barcodes.controller.ts
@@ -1,11 +1,34 @@
-import { Controller, Post, Req, Request } from '@nestjs/common';
+import { Controller, HttpCode, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+const dbr = require("barcode4nodejs");
 
 @Controller('readBarcodes')
 export class ReadBarcodesController {
   @Post()
+  @HttpCode(200)
   async decode(@Req() request: Request) {
-    const json = await request.json();
-    console.log(json);
-    return 'This action returns all cats';
+    const body = request.body;
+    const startTime = (new Date()).getTime();
+    const results = await dbr.decodeBase64Async(body["base64"], dbr.formats.OneD | dbr.formats.PDF417 | dbr.formats.QRCode | dbr.formats.DataMatrix | dbr.formats.Aztec, "");
+    const elapsedTime = (new Date()).getTime() - startTime;
+    const response:any = {};
+    response.results = [];
+    for (let index = 0; index < results.length; index++) {
+      const result = results[index];
+      response.results.push({
+        barcodeText: result.value,
+        barcodeFormat: result.format,
+        x1: result.x1,
+        x2: result.x2,
+        x3: result.x3,
+        x4: result.x4,
+        y1: result.y1,
+        y2: result.y2,
+        y3: result.y3,
+        y4: result.y4
+      })
+    }
+    response.elapsedTime = elapsedTime;
+    return response;
   }
 }
