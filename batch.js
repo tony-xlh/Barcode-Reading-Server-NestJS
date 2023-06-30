83faf00044cc3820f98f5fa983780b6fe5941423
xulihang <xulihanghai@163.com>
2023-06-30 16:23:44 +0800

    update batch.js to use await

--- batch.js
+++ batch.js
@@ -13,33 +13,32 @@ function decodingDone(){
 }
 
 async function batchDecode() {
-  for (let index = 0; index < 100; index++) {
+  for (let index = 0; index < 1000; index++) {
     const response = {};
     const results = [];
     const startTime = (new Date()).getTime();
-    dbr.decodeFileAsync("test.png", dbr.formats.OneD | dbr.formats.PDF417 | dbr.formats.QRCode | dbr.formats.DataMatrix | dbr.formats.Aztec,function(err,barcodeResults){
-      const elapsedTime = (new Date()).getTime() - startTime;
-      count = count + 1;
-      console.log(count);
-      for (let index = 0; index < barcodeResults.length; index++) {
-        const result = barcodeResults[index];
-        results.push({
-          barcodeText: result.value,
-          barcodeFormat: result.format,
-          x1: result.x1,
-          x2: result.x2,
-          x3: result.x3,
-          x4: result.x4,
-          y1: result.y1,
-          y2: result.y2,
-          y3: result.y3,
-          y4: result.y4
-        })
-      }
-      response.elapsedTime = elapsedTime;
-      response.results = results;
-      console.log(response);
-    } ,"");
+    const barcodeResults = await dbr.decodeFileAsync("test.png", dbr.formats.OneD | dbr.formats.PDF417 | dbr.formats.QRCode | dbr.formats.DataMatrix | dbr.formats.Aztec,"");
+    const elapsedTime = (new Date()).getTime() - startTime;
+    count = count + 1;
+    console.log(count);
+    for (let index = 0; index < barcodeResults.length; index++) {
+      const result = barcodeResults[index];
+      results.push({
+        barcodeText: result.value,
+        barcodeFormat: result.format,
+        x1: result.x1,
+        x2: result.x2,
+        x3: result.x3,
+        x4: result.x4,
+        y1: result.y1,
+        y2: result.y2,
+        y3: result.y3,
+        y4: result.y4
+      })
+    }
+    response.elapsedTime = elapsedTime;
+    response.results = results;
+    console.log(response);
   }
 }
 
