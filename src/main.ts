f67bb5fc803fdb6a8ae1ee07817403dcd7f55535
xulihang <xulihanghai@163.com>
2023-06-30 16:51:32 +0800

    update readme

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json } from 'express';
 const dbr = require("barcode4nodejs");
-dbr.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==")
+//dbr.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==")
+dbr.initLicense("t0074oQAAABz1tASEoWT4IIp00emVmVI9CDkIbZtyKBCrSbAZcltlVnqIuM6/r5afZwkP60uzSrBlN5kTWD1Y2IIawWEUYSDZAwjyIxQ=");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
