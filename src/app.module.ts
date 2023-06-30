9df0256cabd08a6b6ad74d6fc78a5288eb691a68
xulihang <xulihanghai@163.com>
2023-06-30 11:24:29 +0800

    serve static files in the client folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { ReadBarcodesController } from './read-barcodes/read-barcodes.controller';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
   controllers: [AppController, ReadBarcodesController],
   providers: [AppService],
 })
